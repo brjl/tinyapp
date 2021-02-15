cbb90090a3cfa71a7a5abc0635ef79037a448f9e
Liam Butler <lrbutler@gmail.com>
2021-02-15 23:29:35 +0000

    added post func for new url and randomstring func

--- express_server.js
+++ express_server.js
@@ -4,11 +4,17 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+const crypto = require("crypto");
+function generateRandomString() {
+  crypto.randomBytes(3).toString('hex');
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,13 +25,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.body.longURL }; //req.body.longURL  or req.params.longURL
   res.render("urls_show", templateVars);  
 });
 
