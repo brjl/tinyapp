cc7665acbf6dfb81d51f39ad63817951f3b71fef
Liam Butler <lrbutler@gmail.com>
2021-02-16 01:41:10 +0000

    non functioning edit buttons index

--- express_server.js
+++ express_server.js
@@ -3,18 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 const crypto = require("crypto");
 function generateRandomString() {
-  return crypto.randomBytes(3).toString('hex');
+  return crypto.randomBytes(3).toString("hex");
 }
 //are we wrapping the url database in a hhttp method? how to route
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,12 +24,11 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log(req.body); // Log the POST request body to the console
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  const shortURL = generateRandomString()
-  urlDatabase[shortURL] = req.body.longURL
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
- 
 });
 
 app.get("/urls/new", (req, res) => {
@@ -39,19 +36,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
